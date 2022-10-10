7ace88341748f067691b3a6d69327fcf9f4a8ab6
zhaoding <zhaoding@agilines.com>
2022-10-10 10:29:07 +0800

    print()函数的使用

---
+++ code/demo1.py
@@ -0,0 +1,23 @@
+# @Organization: 上海敏桥信息科技有限公司
+# @Author      : Kevin
+# @Time        : 2022/10/9 7:47
+# @Function    : print()函数的使用
+
+# 输出字符串
+print(520)
+print(98.5)
+
+# 可以输出字符串
+print('hello')
+print("world")
+
+# 输出含有运算符的表达式
+print(3 + 1)
+
+# 将数据输出文件中
+fp = open('D:/text.txt', 'a+')
+print('hello world', file=fp)
+fp.close()
+
+# 不进行换行输出
+print('hello', 'world', 'python')
