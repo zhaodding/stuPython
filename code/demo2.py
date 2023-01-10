d046d726895de7134b790567e13ff24d9b1e99b3
zhaoding <zhaoding@agilines.com>
2023-01-10 23:03:54 +0800

    print()函数的使用

---
+++ code/demo2.py
@@ -0,0 +1,19 @@
+# @Organization: 上海敏桥信息科技有限公司
+# @Author      : Kevin
+# @Time        : 2022/10/10 10:29
+# @Function    : 转义字符
+
+print('hello\nworld')    #\ +转义功能的首字母  n --> newline的首字符表示换行
+print('hello\tworld')
+print('hellooo\tworld')
+print('hello\rworld')    # world将hello进行了覆盖
+print('hello\bworld')    # \b是退一个格，将o退没了
+
+print('http:\\\\www.baidu.com')
+print('老师说：\'大家好\'')
+
+
+# 原字符，不希望字符串中的转义字符起作用，就使用原字符，就是在字符串之前加上r，或R
+print(r'hello\nworld')
+# 注意事项，最后一个字符不能是反斜杠
+# print(r'hello\nworld\')
