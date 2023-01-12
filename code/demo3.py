6f23d7388f623bef51394093c207f7d8a675d39b
zhaodding <ban_bu@163.com>
2023-01-12 20:05:21 +0800

    1.12 字符串格式化精度控制

---
+++ code/demo3.py
@@ -0,0 +1,10 @@
+# @Organization: 上海莫克信息科技有限公司
+# @Author      : Kevin
+# @Time        : 2023/1/12 19:53 
+# @Function    : 字符串格式化
+
+print('-----------字符串拼接------------')
+
+name = "上海"
+message = "上海莫克科技有限公司，地址在：%s" % name
+print(message)
